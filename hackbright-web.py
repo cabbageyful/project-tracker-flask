499c73e682dc385997fb980a9edf2428e9321fbf
Hackbright Student <info@hackbrightacademy.com>
2016-07-27 13:04:53 -0700

    Added list of projects and grades for each student on student-info page.

--- hackbright-web.py
+++ hackbright-web.py
@@ -12,11 +12,19 @@ def get_student():
     github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
+    project_info = hackbright.get_grades_by_github(github)
+
+    project_info = [{'title': title, 'grade':grade} for title, grade in project_info]
+    
 
     html = render_template('student_info.html',
                             first=first,
                             last=last,
-                            github=github)
+                            github=github,
+                            project_info=project_info
+                            )
+                            
+
     return html
 
 @app.route("/student-search")
